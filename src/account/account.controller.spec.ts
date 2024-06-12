a3136ffef051346c7d7a39b72543024a536c33f3
Joonyeol Oh <87957809+zbqmal@users.noreply.github.com>
2024-06-11 22:50:27 -0400

    feat(RN-004): Sign Up API without JWT (#3)
    * feat(RN-004): sign up api implementation with db store
    
    * feat(RN-004): writes unit tests

--- src/account/account.controller.spec.ts
+++ src/account/account.controller.spec.ts
@@ -1,32 +1,54 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AccountController } from './account.controller';
+import { AccountService } from './account.service';
 
 describe('AccountController', () => {
   let controller: AccountController;
+  let service: AccountService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AccountController],
+      providers: [AccountService],
     }).compile();
 
     controller = module.get<AccountController>(AccountController);
+    service = module.get<AccountService>(AccountService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
 
-  describe('SignIn', () => {
+  describe('signIn', () => {
     it('should return dummy string', () => {
       const result = controller.signIn();
       expect(result).toContain('SignIn');
     });
   });
 
-  describe('SignIn', () => {
-    it('should return dummy string', () => {
-      const result = controller.signUp();
-      expect(result).toContain('SignUp');
+  describe('signUp', () => {
+    it('should create a new account', async () => {
+      const expectedId = 'test-id';
+      const expectedUsername = 'test-username';
+      const expectedPassword = 'test-password';
+
+      jest.spyOn(service, 'createAccount').mockResolvedValueOnce({
+        id: expectedId,
+        username: expectedUsername,
+        password: expectedPassword,
+      });
+
+      const result = await controller.signUp(
+        expectedUsername,
+        expectedPassword,
+      );
+
+      expect(result).toEqual({
+        id: expectedId,
+        username: expectedUsername,
+        password: expectedPassword,
+      });
     });
   });
 });
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AccountController } from './account/account.controller';
+import { AccountModule } from './account/account.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, AccountController],
+  imports: [AccountModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
