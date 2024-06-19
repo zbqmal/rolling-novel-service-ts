2f4ab57c255cd0372c44cae226ed554b4a6e15aa
zbqmal <zbqmal@gmail.com>
2024-06-18 23:43:31 -0400

    feat(RN-005): local environment config setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountModule } from './account/account.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AccountModule],
+  imports: [ConfigModule.forRoot({ envFilePath: '.env.local' }), AccountModule],
   controllers: [AppController],
   providers: [AppService],
 })
