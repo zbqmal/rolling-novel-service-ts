d36a7e62cc500e8830765e7aecbbf90b7bd9da7d
Joonyeol Oh <87957809+zbqmal@users.noreply.github.com>
2024-06-18 23:44:31 -0400

    feat(RN-005): local environment config setup (#4)

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as mongoose from 'mongoose';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  app.enableCors({
+    origin: 'http://localhost:3000', // Frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // Set to true if you need to send cookies with the request
+  });
 
   // MongoDB Connection
   await mongoose.connect('mongodb://localhost:27017/rolling-novel');
 
-  await app.listen(3000);
+  const port = configService.get<number>('PORT') || 3000;
+  await app.listen(port);
 }
 bootstrap();
