47019dd69ea82ef35a9bc66c5fdebf22a10e7074
zbqmal <zbqmal@gmail.com>
2024-07-08 22:57:22 -0400

    feat(RN-007): improves signIn endpoint

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -1,21 +1,21 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AccountService } from './account.service';
 import { SignUpRequestDto } from './dto/sign-up-request.dto';
+import { SignUpResponseDto } from './dto/sign-up-response.dto';
+import { SignInRequestDto } from './dto/sign-in-request.dto';
+import { SignInResponseDto } from './dto/sign-in-response.dto';
 
 @Controller('account')
 export class AccountController {
   constructor(private readonly accountService: AccountService) {}
 
   @Post('signup')
-  async signUp(@Body() request: SignUpRequestDto) {
+  async signUp(@Body() request: SignUpRequestDto): Promise<SignUpResponseDto> {
     return this.accountService.signUp(request);
   }
 
   @Post('signin')
-  async signIn(
-    @Body('username') username: string,
-    @Body('password') password: string,
-  ) {
-    return this.accountService.signIn(username, password);
+  async signIn(@Body() request: SignInRequestDto): Promise<SignInResponseDto> {
+    return this.accountService.signIn(request);
   }
 }
---
+++ src/account/dto/sign-in-request.dto.ts
@@ -0,0 +1,3 @@
+import { SignUpRequestDto } from './sign-up-request.dto';
+
+export class SignInRequestDto extends SignUpRequestDto {}
