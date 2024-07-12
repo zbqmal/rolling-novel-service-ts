9444185de3d12271d769b92875ec1a54f3a483da
zbqmal <zbqmal@gmail.com>
2024-07-11 23:50:27 -0400

    feat(RN-008): improves unit tests

--- src/account/account.service.spec.ts
+++ src/account/account.service.spec.ts
@@ -11,6 +11,8 @@ import {
   BadRequestException,
   ConflictException,
   InternalServerErrorException,
+  NotFoundException,
+  UnauthorizedException,
 } from '@nestjs/common';
 
 jest.mock('bcryptjs', () => ({
@@ -260,19 +262,14 @@ describe('AccountService', () => {
   describe('signIn', () => {
     describe('success', () => {
       beforeEach(async () => {
-        mockAccountModel = {
-          findOne: jest.fn().mockImplementation(({ username }) => ({
+        mockAccountModel = createMockAccountModel({
+          findOne: jest.fn().mockImplementation(() => ({
             _id: expectedUserId,
-            username: username,
+            username: expectedUsername,
             password: expectedHashedPassword,
           })),
-        };
-        const module = await createTestModule(mockAccountModel);
-        accountService = (module as TestingModule).get<AccountService>(
-          AccountService,
-        );
-        jwtService = (module as TestingModule).get<JwtService>(JwtService);
-        (bcrypt.compare as jest.Mock).mockResolvedValue(true);
+        });
+        await createModule();
       });
 
       it('should return an access token properly', async () => {
@@ -280,6 +277,7 @@ describe('AccountService', () => {
           username: expectedUsername,
           password: expectedPassword,
         };
+        (bcrypt.compare as jest.Mock).mockResolvedValue(true);
 
         const result = await accountService.signIn(signInRequestDto);
 
@@ -292,57 +290,90 @@ describe('AccountService', () => {
     });
 
     describe('failure', () => {
-      describe('when account does not exist in db', () => {
-        beforeEach(async () => {
-          mockAccountModel = {
-            findOne: jest.fn().mockReturnValue(null),
-          };
-          const module = await createTestModule(mockAccountModel);
-          accountService = (module as TestingModule).get<AccountService>(
-            AccountService,
-          );
-          jwtService = (module as TestingModule).get<JwtService>(JwtService);
-          (bcrypt.compare as jest.Mock).mockResolvedValue(true);
+      describe('BadRequestException', () => {
+        describe('When username does not exist in request', () => {
+          beforeEach(async () => {
+            mockAccountModel = createMockAccountModel();
+            await createModule();
+          });
+
+          it('should throw BadRequestException', async () => {
+            const signInRequestDto: SignInRequestDto = {
+              username: '',
+              password: expectedPassword,
+            };
+
+            await expect(
+              accountService.signIn(signInRequestDto),
+            ).rejects.toThrow(BadRequestException);
+          });
         });
 
-        it('should throw an error', async () => {
-          const signInRequestDto: SignInRequestDto = {
-            username: expectedUsername,
-            password: expectedPassword,
-          };
+        describe('When password does not exist in request', () => {
+          beforeEach(async () => {
+            mockAccountModel = createMockAccountModel();
+            await createModule();
+          });
 
-          await expect(accountService.signIn(signInRequestDto)).rejects.toThrow(
-            'Invalid credentials',
-          );
+          it('should throw BadRequestException', async () => {
+            const signInRequestDto: SignInRequestDto = {
+              username: expectedUsername,
+              password: '',
+            };
+
+            await expect(
+              accountService.signIn(signInRequestDto),
+            ).rejects.toThrow(BadRequestException);
+          });
         });
       });
 
-      describe('when password does not match', () => {
-        beforeEach(async () => {
-          mockAccountModel = {
-            findOne: jest.fn().mockImplementation(({ username }) => ({
-              _id: expectedUserId,
-              username: username,
-              password: expectedHashedPassword,
-            })),
-          };
-          const module = await createTestModule(mockAccountModel);
-          accountService = (module as TestingModule).get<AccountService>(
-            AccountService,
-          );
-          jwtService = (module as TestingModule).get<JwtService>(JwtService);
-          (bcrypt.compare as jest.Mock).mockResolvedValue(false);
+      describe('NotFoundException', () => {
+        describe('When there is no account matching', () => {
+          beforeEach(async () => {
+            mockAccountModel = createMockAccountModel({
+              findOne: jest.fn().mockReturnValueOnce(null),
+            });
+            await createModule();
+          });
+
+          it('should throw NotFoundException', async () => {
+            const signInRequestDto: SignInRequestDto = {
+              username: expectedUsername,
+              password: expectedPassword,
+            };
+
+            await expect(
+              accountService.signIn(signInRequestDto),
+            ).rejects.toThrow(NotFoundException);
+          });
         });
+      });
 
-        it('should throw an error', async () => {
-          const signInRequestDto: SignInRequestDto = {
-            username: expectedUsername,
-            password: expectedPassword,
-          };
+      describe('UnauthorizedException', () => {
+        describe('When password does not match with DB', () => {
+          beforeEach(async () => {
+            mockAccountModel = createMockAccountModel({
+              findOne: jest.fn().mockImplementation(() => ({
+                _id: expectedUserId,
+                username: expectedUsername,
+                password: expectedHashedPassword,
+              })),
+            });
+            await createModule();
+          });
 
-          await expect(accountService.signIn(signInRequestDto)).rejects.toThrow(
-            'Invalid credentials',
-          );
+          it('should throw UnauthorizedException', async () => {
+            const signInRequestDto: SignInRequestDto = {
+              username: expectedUsername,
+              password: expectedPassword,
+            };
+            (bcrypt.compare as jest.Mock).mockResolvedValue(false);
+
+            await expect(
+              accountService.signIn(signInRequestDto),
+            ).rejects.toThrow(UnauthorizedException);
+          });
         });
       });
     });
